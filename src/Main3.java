c6cb56c7f9f2f0563e825cc720683b6f196ec6e3
Slawaleks <vgalekseew@gmail.com>
2022-11-22 14:06:35 +0300

    22

--- src/Main3.java
+++ src/Main3.java
@@ -4,22 +4,23 @@ public class Main3 {
         int[] nums1 = new int[]{-2, 5, -7, 16, -9, 77, -16};
         int nums2 = 0;
         for (int b = 0; b < nums1.length; b++) {
-            if (nums1[b]%2 != 0) nums2++;
+            if (nums1[b] % 2 != 0) nums2++;
         }
         System.out.println("Количество нечетных чисел в этом массиве: " + nums2);
 
         int nums3 = 0;
         for (int b = 0; b < nums1.length; b++) {
-            if (nums1[b]%2 == 0) nums3++;
+            if (nums1[b] % 2 == 0) nums3++;
         }
         System.out.println("Количество четных чисел в этом массиве: " + nums3);
         int nums4 = 0;
         for (int b = 0; b < nums1.length; b++) {
-            if (nums1[b]%2 != 0) nums4++; if (nums1[b] < 0) nums4++;}
-            System.out.println("Количество отрицательных и нечетных чисел в этом массиве: " + nums4);
-        int c = nums3+nums2;
-        System.out.println("Общее количество чисел: " + c);
+            if ((nums1[b] % 2 != 0) && (nums1[b] < 0)) nums4++;
 
+                    }
+        System.out.println("Количество отрицательных и нечетных чисел в этом массиве: " + nums4);
+        int c = nums3 + nums2;
+        System.out.println("Общее количество чисел: " + c);
 
 
     }
