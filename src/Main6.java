ee9ffb7c5a7f453670da9061b5db275b02d09ddc
Slawaleks <vgalekseew@gmail.com>
2023-01-31 16:08:10 +0300

    Ура!! Подсчитал 10-ки

--- src/Main6.java
+++ src/Main6.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main6 {
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 1;
+        arr[1] = 10;
+        arr[2] = 2;
+        arr[3] = 10;
+        int i = 0;
+        int sum = 0;
+
+        while (true) {
+            if (arr[i] != 10) {
+                if (i == 3) break;
+                i++;
+            }
+            if (arr[i] == 10) {
+                sum = sum + 1;
+                if (i == 3) break;
+                i++;
+            }
+
+        }
+        System.out.println("Количество десяток в массиве: " + sum);
+
+    }
 }
